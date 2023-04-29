427679258093bdd2e4cc20344b6dcfe5168eb4ec
Andrea Nerla <andrea.n@lamarzocco.com>
2023-04-29 18:42:57 +0200

    it retrieves the safeness percentage if i cast colour in the function

--- lichess_api_prova.py
+++ lichess_api_prova.py
@@ -4,39 +4,49 @@ import asyncio
 import chess  
 import chess.engine
 import logging
+from dataclasses import dataclass
 
 logging.getLogger().setLevel(logging.INFO) #to display also info in terminal
 
 ###############
+#TYPES
+###############
+@dataclass
+class Colour:
+    colour: str = "black", "white"  
+
+
+###############
 #FUNCS
 ###############
-    
-def response_unpacking(response: dict) -> dict:
-    #takes the response dict with the various variations 
-    parsed_dict = response["pv"] 
-    return parsed_dict
 
-def total_cp_unweighted(response_json: list[dict] | dict) -> dict:
-    #calculates the total cp of a certain move with its n variations.
-     
-    total_cp = 0
- 
-    if isinstance(response_json, dict):
-        return response_json["cp"]
-    else:
-        for d in response_json:
-            total_cp += d["cp"]
+def average_cp(cp_list: list) -> int:
+    #average of a list
 
-    return total_cp 
+    return sum(cp_list) / len(cp_list)
+
+
+def safeness_calculator_unweighted(cp_list: list, colour: Colour) -> int:
+    #calculates the ratio of cp of best move and average cp of its n variations, espressed in %.
+    if colour == "black":
+        cp_list.reverse()
+
+    logging.info(f"cp_list: {cp_list}")
+
+    safeness_perc = round((average_cp(cp_list) / cp_list[0])  * 100, 2)  
+    logging.info(f"safeness_perc: {safeness_perc}")
+    return safeness_perc
+
 
 def uci_moves_to_list(uci_moves: str) -> list:
     str_to_list = uci_moves.split(sep=" ")
     return str_to_list
 
 
-def cp_score_to_int(cp: str) -> int:
-    pass
-
+def cp_score_to_int(variant: dict) -> int:
+    #gets a sf variant, returns cp in int 
+    cp_int = variant['score'].relative.score()
+    return cp_int
 
 engine = chess.engine.SimpleEngine.popen_uci("/usr/games/stockfish")
 
@@ -45,11 +55,9 @@ test_fen = "8/k5p1/1p6/p3P1PP/b7/8/5KP1/8 b - - 0 48"
 board = chess.Board(test_fen)
 info = engine.analyse(board, chess.engine.Limit(depth=10), multipv=5)
 
-#for variant in info:
-#    logging.info(f"Score:{variant}")
+various_evals = [cp_score_to_int(var) for var in info]
 
-info_score = info[1]['score'].relative.score()
-logging.info(f"json_res_first: {info_score}")
-logging.info(type(info_score))
+safeness_test = safeness_calculator_unweighted(various_evals, colour="black")
 
 engine.quit()
+
