f2bea0ab84dad0c175eff2a58d1a0ce8d5ddb174
Andrea Nerla <andrea.n@lamarzocco.com>
2023-04-29 17:37:49 +0200

    added stockfish eval, got to retrieve to score as an int

--- lichess_api_prova.py
+++ lichess_api_prova.py
@@ -1,6 +1,8 @@
 import requests
-import numpy as np
+#import numpy as np
+import asyncio
 import chess  
+import chess.engine
 import logging
 
 logging.getLogger().setLevel(logging.INFO) #to display also info in terminal
@@ -8,18 +10,10 @@ logging.getLogger().setLevel(logging.INFO) #to display also info in terminal
 ###############
 #FUNCS
 ###############
-
-def lichess_eval_extract(fen: str) -> dict:
-    api_url = f"https://lichess.org/api/cloud-eval?fen={fen}&depth=22&multiPv=5"
-    print(api_url)
-    response = requests.get(api_url)
-    print(response)
-    # print(response.json())
-    return response.json()
     
 def response_unpacking(response: dict) -> dict:
     #takes the response dict with the various variations 
-    parsed_dict = response["pvs"] 
+    parsed_dict = response["pv"] 
     return parsed_dict
 
 def total_cp_unweighted(response_json: list[dict] | dict) -> dict:
@@ -39,32 +33,23 @@ def uci_moves_to_list(uci_moves: str) -> list:
     str_to_list = uci_moves.split(sep=" ")
     return str_to_list
 
-###############
-###############
 
-fen = "8/6p1/2k4p/5p2/p1p5/P1K1P2P/1P4P1/8 b - - 1 48"
+def cp_score_to_int(cp: str) -> int:
+    pass
 
-eval_test = lichess_eval_extract(fen)
 
-board = chess.Board(fen)
-logging.info(f"board: {board}")
+engine = chess.engine.SimpleEngine.popen_uci("/usr/games/stockfish")
 
-json_response = response_unpacking(eval_test)
+test_fen = "8/k5p1/1p6/p3P1PP/b7/8/5KP1/8 b - - 0 48"
 
-json_res_first = json_response[1]
-print(f"json_res_first: {json_res_first}")
+board = chess.Board(test_fen)
+info = engine.analyse(board, chess.engine.Limit(depth=10), multipv=5)
 
-temp_res = total_cp_unweighted(json_res_first)
-print(temp_res)
+#for variant in info:
+#    logging.info(f"Score:{variant}")
 
-moves_list = uci_moves_to_list(json_res_first["moves"])
-logging.info(f"moves_list: {moves_list}")
+info_score = info[1]['score'].relative.score()
+logging.info(f"json_res_first: {info_score}")
+logging.info(type(info_score))
 
-best_var = chess.Move.from_uci(moves_list[0])
-logging.info(f"best_var: {best_var}")
-
-board.push(best_var) #best_var
-logging.info(f"board_2: {board}")
-
-best_var_fen = board.fen()
-logging.info(f"best_var_fen: {best_var_fen}")
+engine.quit()
