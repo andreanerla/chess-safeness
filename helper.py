9d5c82ef617cd46d3de2a3ad788b86c0bd85864d
Andrea Nerla <andrea.n@lamarzocco.com>
2023-05-06 17:39:56 +0200

    added variation from info

--- helper.py
+++ helper.py
@@ -2,6 +2,8 @@
 import logging
 from dataclasses import dataclass
 from enum import Enum
+from numpy import std, mean
+from chess import engine, Board, Move
 
 logging.getLogger().setLevel(logging.INFO) #to display also info in terminal
 
@@ -22,31 +24,6 @@ def average_cp(cp_list: list) -> int:
     #average of a list
 
     return sum(cp_list) / len(cp_list)
-
-
-def safeness_calculator_plain_difference(cp_list: list, colour: Colour) -> int:
-    logging.info(f"cp_list: {cp_list}")
-
-    plain_diff = cp_list[0] - cp_list[len(cp_list) - 1] 
-    logging.info(f"plain_diff: {plain_diff}")
-
-    return plain_diff
-
-
-def safeness_calculator_unweighted(cp_list: list, colour: Colour) -> int:
-    #calculates the ratio of cp of best move and average cp of its n variations, espressed in %.
-
-    logging.info(f"cp_list: {cp_list}")
-    try:
-        safeness_perc = round((average_cp(cp_list) / cp_list[0])  * 100, 2)  
-    except ZeroDivisionError:
-        safeness_perc = round((average_cp(cp_list) / 0.01)  * 100, 2) #fix
-
-    logging.info(f"numerator; {average_cp(cp_list) / 0.01}")
-
-    logging.info(f"safeness_perc: {safeness_perc}")
-    return safeness_perc
-
 
 def uci_moves_to_list(uci_moves: str) -> list:
     str_to_list = uci_moves.split(sep=" ")
@@ -69,3 +46,55 @@ def evals_ordering(evals: list, colour: Colour) -> list:
     if colour == Colour.black:
         evals.reverse()
     return evals
+
+
+
+
+###############
+#SAFENESS EVALUATION FUNCS
+###############
+
+def safeness_calculator_plain_difference(cp_list: list) -> int:
+    logging.info(f"cp_list: {cp_list}")
+
+    plain_diff = cp_list[0] - cp_list[len(cp_list) - 1] 
+    logging.info(f"plain_diff: {plain_diff}")
+
+    return plain_diff
+
+
+def safeness_calculator_unweighted(cp_list: list) -> int:
+    #calculates the ratio of cp of best move and average cp of its n variations, espressed in %.
+
+    logging.info(f"cp_list: {cp_list}")
+    try:
+        safeness_perc = round((average_cp(cp_list) / cp_list[0])  * 100, 2)  
+    except ZeroDivisionError:
+        safeness_perc = round((average_cp(cp_list) / 0.01)  * 100, 2) #fix
+
+    logging.info(f"numerator; {average_cp(cp_list) / 0.01}")
+
+    logging.info(f"safeness_perc: {safeness_perc}")
+    return safeness_perc
+
+def safeness_cx_variance(cp_lint:list[int]) -> float:
+    standard_dev = std(cp_lint)
+    average = mean(cp_lint)
+    cx_variance = standard_dev / average 
+    logging.info(f"standard_dev: {standard_dev}")
+    logging.info(f"cx_variance: {cx_variance}")
+    return cx_variance
+
+
+###############
+#SUCCESSIVE EVALS FUNCS
+###############
+
+def variation_from_info(infos: list, var_number: int) -> list[Move]:
+    specific_var = infos[var_number]["pv"]
+    logging.info(f"specific_var: {specific_var}")
+    return specific_var
+
+def board_variation(board: Board, variation: list) -> Board: #check if it works
+    board_w_variation = Board.push_san(variation)
+    return board_w_variation
--- lichess_api_prova.py
+++ lichess_api_prova.py
@@ -1,7 +1,8 @@
-from helper import evals_ordering, colour_from_fen, average_cp, safeness_calculator_plain_difference, safeness_calculator_unweighted, uci_moves_to_list, cp_score_to_int  
+from helper import variation_from_info, safeness_cx_variance, evals_ordering, colour_from_fen, average_cp, safeness_calculator_plain_difference, safeness_calculator_unweighted, uci_moves_to_list, cp_score_to_int  
 from helper import Colour
 from chess import engine, Board
 import chess
+import logging
 
 if __name__ == "__main__":
     engine = engine.SimpleEngine.popen_uci("/usr/games/stockfish")
@@ -10,19 +11,21 @@ if __name__ == "__main__":
     draw_fen = "8/7k/8/7P/8/8/3K4/8 w - - 0 1"
     king_gambit_fen = "rnbqkbnr/pppp1ppp/8/4p3/4PP2/8/PPPP2PP/RNBQKBNR b KQkq - 0 2"
 
-    board = Board(test_fen)
+    board = Board(king_gambit_fen)
     info = engine.analyse(board, chess.engine.Limit(depth=15), multipv=5)
 
+    var_1 = variation_from_info(info, 1)
+
     various_evals = [cp_score_to_int(var) for var in info]
-
-
-    # safeness_test = safeness_calculator_unweighted(various_evals, colour="black")
 
     colour = colour_from_fen(test_fen)
 
     ordered_evals = evals_ordering(various_evals, colour)
+    logging.info(f"ordered_evals: {ordered_evals}")
 
-    safeness_plain_diff_test = safeness_calculator_plain_difference(various_evals)
+    safeness_std_res = safeness_cx_variance(ordered_evals)
+
+#    board_variation_test_fen = board_variation() 
 
     engine.quit()
 
